916c6c534da36590f491e895448d6443f255def2
zrichy4307 <65582886+zrichy4307@users.noreply.github.com>
2020-06-01 15:54:59 -0400

    Add files via upload

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,28 @@
+var A = "This is a window alert box"; //created an alert window
+window.alert(A);
+
+
+document.write("lisa told bart, \"knock it off, bart! Or I'll tell dad!\"");
+
+var B = "Concatenated";
+var C = " String" 
+document.write(B + C); //created a concatenated string
+
+var Family = "The Richmonds"; //Created a variable named Family with a value of the Richmonds
+var Family = Family.fontcolor("salmon"); //changed font color to salmon
+
+var Husband = "Zach"; //Created a variable named Husband with a value of Zach
+var Husband = Husband.fontcolor("teal"); //changed font color to teal
+
+var Wife = "Michelle"; //Created a variable named Wife with the value of Michelle
+var Wife = Wife.fontcolor("blue"); //changed font color to blue
+
+var Pets = "Rizzo and Wrigley"; //..Created a variable named Pets with a value of Rizzo and Wrigley
+var Pets = Pets.fontcolor("green"); //Changed font color for variable to green
+document.write(Husband)
+
+document.write("3+3"); //made an expression
+
+
+
+
