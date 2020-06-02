0e5abe972da8b8d6c650cd5489eb473c9e890c70
zrichy4307 <65582886+zrichy4307@users.noreply.github.com>
2020-06-01 21:10:00 -0400

    Add files via upload

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_3.js
@@ -0,0 +1,15 @@
+function Addition_Function() {
+    var addition= 2+4;
+    document.getElementById('Math').innerHTML= "2+4="+addition;   
+}
+
+function Subtraction_Function() {
+    var subtraction=17-32;
+    document.getElementById("Sub").innerHTML="17-32="+subtraction;
+}
+
+function Mult_Divide_Function() {
+    var md=5*5/3;
+    document.getElementById("Math1").innerHTML="5 multiplied by 5 and then divided by 3="+md;
+}
+window.alert(Math.random()*100);
