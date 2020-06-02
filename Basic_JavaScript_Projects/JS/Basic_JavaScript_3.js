c3665a1a4c9918648ff07967f2b46556c1317c1a
zrichy4307 <65582886+zrichy4307@users.noreply.github.com>
2020-06-02 15:42:54 -0400

    Add files via upload

--- Basic_JavaScript_Projects/JS/Basic_JavaScript_3.js
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_3.js
@@ -12,4 +12,17 @@ function Mult_Divide_Function() {
     var md=5*5/3;
     document.getElementById("Math1").innerHTML="5 multiplied by 5 and then divided by 3="+md;
 }
+
+function modulus() {
+    var remainder=15%4;
+    document.getElementById("mod").innerHTML="The remainder of 15 divided by 4="+ remainder;
+}
+
+
+var x=5;
+x++;
+document.write(x);
+
+
+
 window.alert(Math.random()*100);
