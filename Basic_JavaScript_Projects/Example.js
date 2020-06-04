0ce44e6edf6e92cdb601e34711e35fcc7f32ae20
zrichy4307 <65582886+zrichy4307@users.noreply.github.com>
2020-06-04 19:02:46 -0400

    Add files via upload

---
+++ Basic_JavaScript_Projects/Example.js
@@ -0,0 +1,12 @@
+function my_Function() {
+    document.getElementById("Test").innerHTML = 0/0;
+}
+
+function my_Function1() {
+    document.getElementById("Test1").innerHTML = isNaN("This is a string");
+}
+
+
+function my_Function2() {
+    document.getElementById("Test2").innerHTML = isNaN("007");
+}
---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_5.js
@@ -0,0 +1,33 @@
+document.write(typeof "3");
+
+function Infinite() {
+    document.getElementById("Infinity").innerHTML = (2E310);
+}
+
+function Infinite_minus() {
+    document.getElementById("-Infinity").innerHTML = (-2E310);
+}
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20>10);
+}
+
+function not_Function1() {
+    document.getElementById("Not1").innerHTML = !(8>10);
+}
+
+console.log(5+17);
+
+console.log(5>6);
+
+console.log(10+5==15);
+
+console.log(10==6);
+
+console.log(5>4 && 6>5);
+
+console.log(5>7 && 5>1);
+
+console.log(5<4 || 5<6);
+
+console.log(5<4 || 17<12);
